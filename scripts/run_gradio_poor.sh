9c5e6cc2953d4918beaa04c4cd4813116f94bd74
jonathan <jonathan@goava.ai>
2025-06-11 15:51:24 +0000

    update requirement

--- scripts/run_gradio_poor.sh
+++ scripts/run_gradio_poor.sh
@@ -3,8 +3,9 @@ JOBS_DIR=$(dirname $(dirname "$0"))
 export PYTHONPATH=./
 
 export MODEL_BASE=./weights
+export DISABLE_SP=1
 
-checkpoint_path=${MODEL_BASE}/ckpts/hunyuan-video-t2v-720p/transformers/mp_rank_00_model_states.pt
+checkpoint_path=${MODEL_BASE}/ckpts/hunyuan-video-t2v-720p/transformers/mp_rank_00_model_states_fp8.pt
 
 
 torchrun --nnodes=1 --nproc_per_node=1 --master_port 29605 hymm_gradio/flask_audio.py \
@@ -16,7 +17,10 @@ torchrun --nnodes=1 --nproc_per_node=1 --master_port 29605 hymm_gradio/flask_aud
     --cfg-scale 7.5 \
     --infer-steps 50 \
     --use-deepcache 1 \
-    --flow-shift-eval-video 5.0  &
+    --flow-shift-eval-video 5.0 \
+    --use-fp8 \
+    --text-encoder-precision-2 fp16 \
+    --infer-min &
 
 
-python3 hymm_gradio/gradio_audio.py
+python3 hymm_gradio/gradio_audio.py 
